5ef3337dc2389c413c5626c17f5eab630cad6879
Jayrene1 <43413926+Jayrene1@users.noreply.github.com>
2019-03-19 23:13:05 -0400

    initial site files

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = 3000;
+
+var app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
