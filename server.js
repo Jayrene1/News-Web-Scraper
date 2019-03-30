840e39654ac7d0928cdfb0bb76c5d3f3acb9cef3
Jayrene1 <43413926+Jayrene1@users.noreply.github.com>
2019-03-30 15:07:00 -0400

    modified mongodb URI in server.js to connect to mLab db

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var mongoose = require("mongoose");
 
 var PORT = 3000;
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsscraperdb";
 
 var app = express();
 
@@ -10,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/newsscraperdb", { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
